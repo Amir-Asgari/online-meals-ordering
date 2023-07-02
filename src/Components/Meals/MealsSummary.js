948b6e8894bf78db142fd821d475709078798806
aghay-marco <mr.a.asgari@gmail.com>
2023-07-02 23:35:05 +0330

    add context

--- src/Components/Meals/MealsSummary.js
+++ src/Components/Meals/MealsSummary.js
@@ -4,18 +4,18 @@ import classes from './MealsSummary.module.css'
 
 function MealsSummary(props) {
     return (
-        <section >
-        <div className={classes.card}  >
-            <h2>Delicious Food, Delivered To You</h2>
-            <p>
-                Choose your favorite meal from our broad selection of available meals
-                and enjoy a delicious lunch or dinner at home.
-            </p>
-            <p>
-                All our meals are cooked with high-quality ingredients, just-in-time and
-                of course by experienced chefs!
-            </p>
-        </div>
+        <section className={classes.section}>
+            <div className={classes.card}  >
+                <h2>Delicious Food, Delivered To You</h2>
+                <p>
+                    Choose your favorite meal from our broad selection of available meals
+                    and enjoy a delicious lunch or dinner at home.
+                </p>
+                <p>
+                    All our meals are cooked with high-quality ingredients, just-in-time and
+                    of course by experienced chefs!
+                </p>
+            </div>
         </section>
 
     )
