88b2fb8eb35e64b36bb50db6172925109c4787c8
aghay-marco <97605439+aghay-marco@users.noreply.github.com>
2023-07-06 11:37:52 +0330

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import Cart from './../src/Components/Cart/Cart'
