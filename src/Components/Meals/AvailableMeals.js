7f8be367a6b63159e887db986cbb9d43b3a52fc7
amir-asgari <mr.a.asgari@gmail.com>
2023-08-06 11:44:12 +0330

    add pictures to the Assets Folder add Animation on the meals photo and add button -- mealItem.css and mealItemFor.css

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -2,56 +2,65 @@ import React, { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
+import sushi from "../../Assets/sushi.jpg";
+import pasta from "../../Assets/pasta.jpg";
+import fish from "../../Assets/fish-and-chips.jpg";
+import past from "../../Assets/pasta.jpg";
+
 const Dummy_meals = [
   {
     id: "m1",
     name: "Sushi",
     description: "Finest fish and veggies",
     price: 22.99,
+    image:sushi,
   },
   {
     id: "m2",
     name: "Schnitzel",
     description: "A german specialty!",
     price: 16.5,
+    image: pasta,
   },
   {
     id: "m3",
     name: "Sushi2",
     description: "Finest fish and veggies",
     price: 22.99,
+    image:fish ,
   },
   {
     id: "m4",
     name: "Schnitzel3",
     description: "A german specialty!",
     price: 16.5,
+    image:sushi,
   },
 ];
 
 function AvailableMeals() {
   const [meals, setMeals] = useState([]);
-//   useEffect(() => {
-//     const fetchMeals = async () => {
-//       const response = await fetch(
-//         "https://react-http-6b4a6.firebase.com/meals.json"
-//       );
-//       const responseData = await response.json();
+  //   useEffect(() => {
+  //     const fetchMeals = async () => {
+  //       const response = await fetch(
+  //         "https://react-http-6b4a6.firebase.com/meals.json"
+  //       );
+  //       const responseData = await response.json();
 
-//       const loadedMeals = [];
+  //       const loadedMeals = [];
 
-//       for (const key in responseData) {
-//         loadedMeals.push({
-//           id: key,
-//           name: responseData[key].name,
-//           description: responseData[key].description,
-//           price: responseData[key].price,
-//         });
-//       }
-//       setMeals(loadedMeals);
-//     };
-//     fetchMeals();
-//   }, []);
+  //       for (const key in responseData) {
+  //         loadedMeals.push({
+  //           id: key,
+  //           name: responseData[key].name,
+  //           description: responseData[key].description,
+  //           price: responseData[key].price,
+  //         });
+  //       }
+  //       setMeals(loadedMeals);
+  //     };
+  //     fetchMeals();
+  //   }, []);
 
   const Meals = Dummy_meals.map((meal) => (
     <MealItem
@@ -60,12 +69,13 @@ function AvailableMeals() {
       name={meal.name}
       description={meal.description}
       price={meal.price}
+      image={meal.image}
     />
   ));
   return (
     <section>
       <Card>
-        <ul style={{ with: "90%"}}>{Meals} </ul>
+        <ul style={{ with: "90%" }}>{Meals} </ul>
       </Card>
     </section>
   );
