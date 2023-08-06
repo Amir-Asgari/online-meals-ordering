69afc95bbbe04d2815996c1cf231f47bf84b87b0
amir-asgari <mr.a.asgari@gmail.com>
2023-08-06 14:37:39 +0330

    add fetch data instead of dummyData --AvailableMeals.js

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,24 +1,26 @@
-import classes from './CartItem.module.css'
+import classes from "./CartItem.module.css";
 
-const CartItem = props => {
-    const price = `$${props.price.toFixed(2)}`;
+const CartItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
 
-    return (
-        <li className={classes['cart-item']}>
-            <div>
-                <h2>{props.name}</h2>
-                <div className={classes.summary}>
-                    <span className={classes.price}>{price}</span>
-                    <span className={classes.amount}>X{props.amount}</span>
-                </div>
-            </div>
-            <div className={classes.actions}>
-                <button onClick={props.onRemove}>-</button>
-                <button onClick={props.onAdd}>+</button>
-            </div>
-        </li>
-
-    )
-}
+  return (
+    <li className={classes["cart-item"]}>
+      <div className={classes.imageItems}>
+        <img className={classes.image} src={props.image} />
+      </div>
+      <div>
+        <h2>{props.name}</h2>
+        <div className={classes.summary}>
+          <span className={classes.price}>{price}</span>
+          <span className={classes.amount}>X{props.amount}</span>
+        </div>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={props.onRemove}>-</button>
+        <button onClick={props.onAdd}>+</button>
+      </div>
+    </li>
+  );
+};
 
 export default CartItem;
--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -7,62 +7,62 @@ import pasta from "../../Assets/pasta.jpg";
 import fish from "../../Assets/fish-and-chips.jpg";
 import past from "../../Assets/pasta.jpg";
 
-const Dummy_meals = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-    image:sushi,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-    image: pasta,
-  },
-  {
-    id: "m3",
-    name: "Sushi2",
-    description: "Finest fish and veggies",
-    price: 22.99,
-    image:fish ,
-  },
-  {
-    id: "m4",
-    name: "Schnitzel3",
-    description: "A german specialty!",
-    price: 16.5,
-    image:sushi,
-  },
-];
+// const Dummy_meals = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//     image:sushi,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//     image: pasta,
+//   },
+//   {
+//     id: "m3",
+//     name: "Sushi2",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//     image:fish ,
+//   },
+//   {
+//     id: "m4",
+//     name: "Schnitzel3",
+//     description: "A german specialty!",
+//     price: 16.5,
+//     image:sushi,
+//   },
+// ];
 
 function AvailableMeals() {
   const [meals, setMeals] = useState([]);
-  //   useEffect(() => {
-  //     const fetchMeals = async () => {
-  //       const response = await fetch(
-  //         "https://react-http-6b4a6.firebase.com/meals.json"
-  //       );
-  //       const responseData = await response.json();
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-23a17-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+        console.log(responseData);
+      const loadedMeals = [];
 
-  //       const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
 
-  //       for (const key in responseData) {
-  //         loadedMeals.push({
-  //           id: key,
-  //           name: responseData[key].name,
-  //           description: responseData[key].description,
-  //           price: responseData[key].price,
-  //         });
-  //       }
-  //       setMeals(loadedMeals);
-  //     };
-  //     fetchMeals();
-  //   }, []);
-
-  const Meals = Dummy_meals.map((meal) => (
+  const Meals = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
